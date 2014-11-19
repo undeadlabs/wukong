61bb54bcbfa0a4e189f3344b7d9cc5e6e312f87e
Ben Scott <gamepoet@gmail.com>
2014-11-19 14:45:36 -0800

    make Dataflow quack like a Processor

--- lib/wukong/dataflow.rb
+++ lib/wukong/dataflow.rb
@@ -4,15 +4,63 @@ module Wukong
     def self.configure(settings)
       settings.description = builder.description if builder.description
     end
+
+    def setup
+      each_stage do |stage|
+        stage.setup
+      end
+    end
+
+    def process(record, &emit)
+      process_list(record, [root], &emit)
+    end
+
+    def finalize(&emit)
+      finalize_list([root], &emit)
+    end
+
+    def stop
+      each_stage do |stage|
+        stage.stop
+      end
+    end
+
+  private
+    def process_list(record, stages, &emit)
+      stages.each do |stage|
+        children = descendents(stage)
+        if children.empty?
+          stage.process(record, &emit)
+        else
+          stage.process(record) do |out|
+            process_list(out, children, &emit)
+          end
+        end
+      end
+    end
+
+    def finalize_list(stages, &emit)
+      stages.each do |stage|
+        children = descendents(stage)
+        if children.empty?
+          stage.finalize(&emit)
+        else
+          stage.finalize do |out|
+            process_list(out, children, &emit)
+          end
+          finalize_list(children, &emit)
+        end
+      end
+    end
   end
-  
+
   class DataflowBuilder < Hanuman::TreeBuilder
 
     def description desc=nil
       @description = desc if desc
       @description
     end
-    
+
     def namespace() Wukong::Dataflow ; end
 
     def handle_dsl_arguments_for(stage, *args, &action)
@@ -30,7 +78,7 @@ module Wukong
       stage.graph = self
       stage
     end
-  
+
     def method_missing(name, *args, &blk)
       if stages[name]
         handle_dsl_arguments_for(stages[name], *args, &blk)
@@ -38,6 +86,6 @@ module Wukong
         super
       end
     end
-    
+
   end
 end
